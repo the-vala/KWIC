54b137300c8f055b72514baff92a159766749819
Diego Valadez <the-vala@users.noreply.github.com>
2019-11-06 19:50:53 -0600

    Classes Declaration

--- main.cpp
+++ main.cpp
@@ -1,16 +1,79 @@
 #include <iostream>
 
 class AbstractWordsContainer {
-    AbstractWordsContainer () {};
+public:
+    AbstractWordsContainer () {}
 };
 
 class VectorWords : public AbstractWordsContainer {
-
+public:
+    VectorWords () {}
 };
 
 class InputProcessor {
-    InputProcessor () {};
-    
+public:
+    InputProcessor () {}
+
+};
+
+class STDIOInput : public InputProcessor {
+public:
+    STDIOInput () {}
+};
+
+class FileIOInput : public InputProcessor {
+public:
+    FileIOInput () {}
+};
+
+class AbstractWordsProcessor {
+public:
+    AbstractWordsProcessor () {}
+};
+
+class WordsProcessor : public AbstractWordsProcessor {
+public:
+    WordsProcessor () {}
+};
+
+class AbstractRotationProcessor {
+public:
+    AbstractRotationProcessor () {}
+};
+
+class RotationProcessor : public AbstractRotationProcessor {
+public:
+    RotationProcessor () {}
+};
+
+class Sort {
+public:
+    Sort () {}
+};
+
+class SortMethod : public Sort {
+public:
+    SortMethod () {}
+};
+
+class MergeSort : public Sort {
+public:
+    MergeSort () {}
+};
+
+class OutputProcessor {
+public:
+    OutputProcessor () {}
+};
+
+class STDIOOutput : public OutputProcessor {
+public:
+    STDIOOutput () {}
+};
+
+class FileIOOutput : public OutputProcessor {
+public:
+    FileIOOutput () {}
 };
 
 int main() {
